9e26d4855c7ff71557b3495a8ffa39ed91bac5b7
MichaelaVop <70598230+MichaelaVop@users.noreply.github.com>
2021-03-06 01:04:03 +0100

    js second commit

--- js/app.js
+++ js/app.js
@@ -1,34 +1,25 @@
 //const MY_WORK = 'data.json'
-// const myWork = document.querySelector('#myWork');
-console.log("js funguje");
-let work = [];
+"use strict";
+const workContainer = document.querySelector('#myWork');
 
-let printMyWork = function (workArray) {
-    let workDiv = document.querySelector('#myWork');
-    workDiv.innerHTML = '';
-    console.log("after print");
-    workArray.forEach(e => {
-        return (
-            document.querySelector('#myWork').innerHTML +=
-            "\n <div class=\"col-md-4\">\n    <div class=\"icon gradient mb-4\">\n  <i class=\"" + e.icon + "\"></i>\n   </div>\n    <h3>" + e.title + "</h3>     <p>" + e.description + "</p>\n    </div>\n "
-            );
-    });
-    console.log("after element each");
-};
-
-let myWorkData = function () {
-    fetch("./data.json")
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data) {
-        work = data;
-        printMyWork(work);
-        console.log("data", work);
-    });
-
+async function getWork() {
+    const res = await fetch("js/data.json");
+    const {results} = await res.json();
+    showWork(results);
 }
 
-console.log("js konec");
+getWork();
+
+function showWork(works) {
+    workContainer.innerHTML = '';
+    works.forEach((work) => {
+        const workEl = document.createElement('div');
+        workEl.className = 'work';
+        workEl.innerHTML = "\n <div class=\"col-md-4\">\n    <div class=\"icon gradient mb-4\">\n  <i class=\"" + e.icon + "\"></i>\n   </div>\n    <h3>" + e.title + "</h3>     <p>" + e.description + "</p>\n    </div>\n"
+        workContainer.appendChild(workEl);
+    })
+}
+console.log("try");
+
 
 
